cbb64a31023cb303ef79d6b7a43a46bbd20b0e45
DannyRuben <108206793+DannyRuben@users.noreply.github.com>
2022-10-27 00:03:48 -0700

    Update API.js

--- src/API.js
+++ src/API.js
@@ -5,7 +5,7 @@ var baseURL;
 // if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
 //     baseURL = process.env.REACT_APP_API_BASE_URL;
 // } else {
-baseURL = 'https://backend-cinemagd.herokuapp.com/';
+baseURL = 'https://cinemapara-back.dannysolorza.repl.co/';
 // }
 
 const api = axios.create({
